0b36462037e1431ca8c9411ec59ca3a2173a35d7
Amy Oglesbee <amyoglesbee@gmail.com>
2026-06-13 16:53:40 -0500

    small corrections in my comments to cryptography hashing lab project

--- projects/cybersecurity-projects/cryptography/hashing/sha256-hash-lab.py
+++ projects/cybersecurity-projects/cryptography/hashing/sha256-hash-lab.py
@@ -1,10 +1,10 @@
-#python library with cryptographic hash functions like SHA-256, SHA-1, MD5, etc which will transform data into a fixed-length fingerprint.
-#fingerprint is deterministic
-#irreversible
-#collision-resistant (hard to find matches)
+#hashlib python library with cryptographic hash functions like SHA-256, SHA-1, MD5, etc which will transform data into a fixed-length hash
+#Deterministic output: same input, same hash. Different input, diff hash.
+#irreversible, one-way - used to verify, not recover
+#Reproducable
 import hashlib
 
-#variable is set to a string value that is unsecured and needs protection.  This info could be exposed, compromised and reused.
+#variable is set to a string value that is unsecured and needs protection. Could be exposed, compromised and reused.
 secret_message="cybersecurity101"
 
 #cryptography in python begins now
@@ -14,14 +14,20 @@ secret_message="cybersecurity101"
 #hashlib.sha256(...) takes bytes and runs it thru sha256 algorithm to produce a hash object - a fixed representation of the input data cybersecurity101
 #hashed_object is a container holding the computed result and methods to display it
 #transformation that happened is irreversible
-#output is retrievable but not human-readable
+#output is retrievable right now but not human-readable
 
 hashed_object=hashlib.sha256(secret_message.encode())
 
 
-#next step is to extract the fingerprint
-#hexdigest() converts binary hash into a readable hexadecimal string
-hex_fingerprint=hashed_object.hexdigest()
+#extract the hash, use hexdigest() to convert binary hash into a readable hexadecimal. Hexadecimal is the standard format used across systems.
+hex_output=hashed_object.hexdigest()
 
 print("Original Text:", secret_message)
-print("SHA-256 Hash :", hex_fingerprint)
+print("SHA-256 Hash :", hex_output)
+
+
+# Example Output:
+# (When secret_message = "cybersecurity101")
+#
+# Original Text: cybersecurity101
+# SHA-256 Hash : 189353bca1b7a01c5116fabd80ffc5d751cd6c7cc417a21ff75cfb51ab060db3
